e3a4a97c53c5b3cc40d7d3d97c1f0218dfc3e2c7
BeachBall2024 <guillermo.filgueirafernandez@uzh.ch>
2026-06-08 06:08:18 +0200

    Prepare final self-contained submission

--- generate_map.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-import pickle
-import pandas as pd
-
-# Let's just generate a dummy map or try to import from the existing script data if possible.
-# Actually, the user asked me to "put those images in a folder". We already have `output_soundmap.png` which is the map!
-# Let me look at what `output_soundmap.png` is.
